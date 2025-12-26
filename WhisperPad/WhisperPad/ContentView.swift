d11681bd0e5fba6a0970b8ea8a569b823107fd14
r0227n <developryo@gmail.com>
2025-12-27 02:27:15 +0900

    chore: init project

---
+++ WhisperPad/WhisperPad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhisperPad
+//
+//  Created by RyoNishimura on 2025/12/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhisperPad/WhisperPad/WhisperPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperPadApp.swift
+//  WhisperPad
+//
+//  Created by RyoNishimura on 2025/12/27.
+//
+
+import SwiftUI
+
+@main
+struct WhisperPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
